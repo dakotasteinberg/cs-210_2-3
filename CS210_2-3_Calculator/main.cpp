033eb0efccb61d916a78341a81a49a2babd97071
dakotasteinberg <60726578+dakotasteinberg@users.noreply.github.com>
2023-01-15 18:22:09 -0600

    Update main.cpp
    Formatted the file with appropriate whitespace.

--- CS210_2-3_Calculator/main.cpp
+++ CS210_2-3_Calculator/main.cpp
@@ -34,8 +34,7 @@ int main() // Changed void to int so the below function will return integer type
 	int op1, op2;
 	char operation;
 	char answer = 'Y'; // Added whitespace around = and ; to end of line. Changed double quotes to single quotes for char type.
-	while ((answer == 'Y') || (answer == 'y')) // Added whitespace around ==. Added 'Y' to while loop allowing for both lower and uppercase characters to be input.
-	{
+	while ((answer == 'Y') || (answer == 'y')) { // Added whitespace around ==. Added 'Y' to while loop allowing for both lower and uppercase characters to be input.
         /*
          * Added brackets to ALL if statements as best practice.
          * Added \n (newline) character to various output statements for nicer formatting during program execution.
@@ -43,24 +42,24 @@ int main() // Changed void to int so the below function will return integer type
 		cout << '\n' << "Enter expression" << endl;
 		cin >> op1 >> operation >> op2; // Switched variables op2/op1 to ensure appropriate program functionality.
 		if (operation == '+') { // Changed double quotes to single quotes for char type. Removed ; from if statement.
-            cout << op1 << " + " << op2 << " = " << op1 + op2 << '\n' << endl; // Changed >> to << for output. Removed ; from if statement.
-        }
+            		cout << op1 << " + " << op2 << " = " << op1 + op2 << '\n' << endl; // Changed >> to << for output. Removed ; from if statement.
+        	}
 		if (operation == '-') {
-            cout << op1 << " - " << op2 << " = " << op1 - op2 << '\n' << endl; // Changed >> to << for output.
-        }
-        if (operation == '*') {
-            cout << op1 << " * " << op2 << " = " << op1 * op2 << '\n' << endl; // Added ; to end of line. Changed / to * to appropriately output expression operator.
-        }
+		    	cout << op1 << " - " << op2 << " = " << op1 - op2 << '\n' << endl; // Changed >> to << for output.
+		}
+		if (operation == '*') {
+		    	cout << op1 << " * " << op2 << " = " << op1 * op2 << '\n' << endl; // Added ; to end of line. Changed / to * to appropriately output expression operator.
+		}
 		if (operation == '/') {
-            cout << op1 << " / " << op2 << " = " << op1 / op2 << '\n' << endl; // Changed * to / to appropriately output expression operator.
-        }
+		    	cout << op1 << " / " << op2 << " = " << op1 / op2 << '\n' << endl; // Changed * to / to appropriately output expression operator.
+		}
 
 		cout << "Do you wish to evaluate another expression?\nY to continue, N to end: " << endl; // Provided user with input options on a new line for clarity.
 		cin >> answer;
 
-        if ((answer == 'N') || (answer == 'n')) { // if statement to providing output when user input N or n for the answer variable.
-            cout << '\n' << "Program Finished." << endl;
-        }
+		if ((answer == 'N') || (answer == 'n')) { // if statement to providing output when user input N or n for the answer variable.
+			cout << '\n' << "Program Finished." << endl;
+        	}
 	}
     return 0; // Indicates successful program execution and completion.
 }
